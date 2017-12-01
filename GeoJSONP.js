2fed2540930746c19c3344ec453f1b8f4f4f05aa
fideljosevillero <fideljose1708@hotmail.com>
2017-12-01 14:12:46 -0500

    Personalizando los markers e infoview

--- GeoJSONP.js
+++ GeoJSONP.js
@@ -5,24 +5,57 @@ eqfeed_callback({
       "geometry": {
         "type": "Point",
         "coordinates": [
-          -75.492554,
-          6.271618        ]
+          6.271618,
+          -75.492554
+        ]
       },
       "id": "1",
       //**
-      "descripcion": "Fidel José Villero De Hoyos"
+      "title": "Titulo 1",
+      "description": "<b>Fidel José Villero De Hoyos 1</b><hr/>ARKIX SA",
+      "image": "./Shikamaru.jpg"
     },
     {
       "geometry": {
         "type": "Point",
         "coordinates": [
-          -75.528259,
-          9.557417
+          9.557417,
+          -75.528259
         ]
       },
       "id": "2",
       //**
-      "descripcion": "Fidel José Villero De Hoyos 2"
+      "title": "Titulo 2",
+      "description": "<b>Fidel José Villero De Hoyos 2</b><hr/>ARKIX SA",
+      "image": "./Shikamaru.jpg"
+    },
+    {
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          4.401183,
+          -69.406128 
+        ]
+      },
+      "id": "3",
+      //**
+      "title": "Titulo 3",
+      "description": "<b>Fidel José Villero De Hoyos 3</b><hr/>ARKIX SA",
+      "image": "./Shikamaru.jpg"
+    },
+    {
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          12.146746,
+          -71.570435  
+        ]
+      },
+      "id": "4",
+      //**
+      "title": "Titulo 4",
+      "description": "<b>Fidel José Villero De Hoyos 4</b><hr/>ARKIX SA",
+      "image": "./Shikamaru.jpg"
     }
   ]
 });
