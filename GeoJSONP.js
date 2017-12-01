ebde1ce96d7f3fc133dc3780def5005f9c5665c4
fideljosevillero <fideljose1708@hotmail.com>
2017-12-01 10:45:12 -0500

    First commit

---
+++ GeoJSONP.js
@@ -0,0 +1,28 @@
+eqfeed_callback({
+  /*"type": "FeatureCollection",*/
+  "features": [
+    {
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -75.492554,
+          6.271618        ]
+      },
+      "id": "1",
+      //**
+      "descripcion": "Fidel José Villero De Hoyos"
+    },
+    {
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -75.528259,
+          9.557417
+        ]
+      },
+      "id": "2",
+      //**
+      "descripcion": "Fidel José Villero De Hoyos 2"
+    }
+  ]
+});
