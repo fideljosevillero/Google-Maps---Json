0180108d752f7363aa6ca67e4487db494eb18cb8
fideljosevillero <fideljose1708@hotmail.com>
2017-12-05 08:55:07 -0500

    Change dynamically value zoom

--- GeoJSONP.js
+++ GeoJSONP.js
@@ -2,14 +2,68 @@ eqfeed_callback({
   /*"type": "FeatureCollection",*/
   "features": [
     {
+      "idCiudad":1,
+      "NombreCiudad":"MedellÃ­n",
+      "idPais":1,
+      "NombrePais":"Colombia",
+      "idZona":1,
+      "NombreZona":"Sur",
+      "idTipo":4,
+      "NombreTipo":"Centro de Servicio",
+      "idPuntoVenta":126,
+      "Nombre":"MedellÃ­n",
+      "Direccion":"Autopista Sur 1-21",
+      "Telefono":"3607157",
+      "Horario":"Diurno",
+      "Latitud":6.20472795,
+      "Longitud":-75.579818
+    },
+    {
+      "idCiudad":2,
+      "NombreCiudad":"MedellÃ­n 2",
+      "idPais":1,
+      "NombrePais":"Colombia 2",
+      "idZona":1,
+      "NombreZona":"Sur 2",
+      "idTipo":4,
+      "NombreTipo":"Centro de Servicio 2",
+      "idPuntoVenta":126,
+      "Nombre":"MedellÃ­n 2",
+      "Direccion":"Autopista Sur 1-21 2",
+      "Telefono":"3607157",
+      "Horario":"Diurno 2",
+      "Latitud":6.26419375,
+      "Longitud":-75.599939
+    },
+    {
+      "idCiudad":1,
+      "NombreCiudad":"MedellÃ­n 3",
+      "idPais":1,
+      "NombrePais":"Colombia 3",
+      "idZona":1,
+      "NombreZona":"Sur 3",
+      "idTipo":4,
+      "NombreTipo":"Centro de Servicio 3",
+      "idPuntoVenta":126,
+      "Nombre":"MedellÃ­n 3",
+      "Direccion":"Autopista Sur 1-21 3",
+      "Telefono":"3607157",
+      "Horario":"Diurno 3",
+      "Latitud":6.2642195,
+      "Longitud":-75.600028
+    }
+
+/*
+
+    {
       "geometry": {
-        "type": "Point",
+        //"type": "Point",
         "coordinates": [
           6.271618,
           -75.492554
         ]
       },
-      "id": "1",
+      //"id": "1",
       //**
       "title": "Titulo 1",
       "description": "<b>Fidel José Villero De Hoyos 1</b><hr/>Texto...",
@@ -56,6 +110,6 @@ eqfeed_callback({
       "title": "Titulo 4",
       "description": "<b>Fidel José Villero De Hoyos 4</b><hr/>Texto...",
       "image": "./Shikamaru.jpg"
-    }
+    } */
   ]
 });
